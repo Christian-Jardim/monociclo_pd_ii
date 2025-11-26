a45f677c63088a7edbe7f124e75acfaeb5c2849c
Christian Jardim <christianjardim.aluno@unipampa.edu.br>
2025-11-26 19:51:50 -0300

    Add files via upload

---
+++ src/pc.vhd
@@ -0,0 +1,18 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+entity PC is
+ port(
+  pc_next : in std_logic_vector(3 downto 0);
+  pc_out : out std_logic_vector(3 downto 0)
+ );
+end entity;
+
+architecture behavior of PC is
+begin
+
+ 
+
+end architecture;

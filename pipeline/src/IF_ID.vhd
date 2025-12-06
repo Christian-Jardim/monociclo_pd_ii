4c3b976e5c10c93e2af0af7b52a437927320a12b
Christian Jardim <christianjardim.aluno@unipampa.edu.br>
2025-12-06 16:38:44 -0300

    Add files via upload

---
+++ pipeline/src/IF_ID.vhd
@@ -0,0 +1,37 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+entity IF_ID is
+ port(
+  clock, reset : in std_logic;
+  pc : in std_logic_vector(7 downto 0);
+  inst : in std_logic_vector(15 downto 0);
+  inst_out : out std_logic_vector(15 downto 0);
+  pc_out : out std_logic_vector(7 downto 0)
+ );
+end entity;
+
+architecture behavior of IF_ID is
+
+ signal inst_s : std_logic_vector(15 downto 0);
+ signal pc_s : std_logic_vector(7 downto 0);
+
+begin
+
+ process(clock, reset)
+ begin
+  if reset = '1' then
+   pc_s <= (others => '0');
+   inst_s <= (others => '0');
+  elsif rising_edge(clock) then
+   pc_s <= pc;
+   inst_s <= inst;
+  end if;
+ end process;
+ 
+ pc_out <= pc_s;
+ inst_out <= inst_s;
+ 
+end architecture;
---
+++ pipeline/src/tb_pipeline.vhd
@@ -0,0 +1,22 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.std_logic_arith.all;
+
+entity tb_pipeline is
+end entity;
+
+architecture behavior of tb_pipeline is
+ signal tb_clock : std_logic:= '0';
+ signal tb_reset : std_logic:= '1';
+begin
+
+ sim_pipeline : entity work.top_SinglecycleProcessor
+ port map(
+  clock => tb_clock,
+  reset => tb_reset
+ );
+
+ tb_reset <= '0' after 15 ps;
+ tb_clock <= not tb_clock after 10 ps;
+end architecture;
